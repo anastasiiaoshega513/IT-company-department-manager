49a74a398602496b7210c69f61afa27db94edcaf
Steicy <anastasiaosega@gmail.com>
2026-01-29 20:38:28 +0200

    created main page index(raw)

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,3 +1,16 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def index(request: HttpRequest) -> HttpResponse:
+    # num_of_books = Book.objects.count()
+    # num_of_authors = Author.objects.count()
+    # num_of_literary_formats = LiteraryFormat.objects.count()
+    # num_visits = request.session.get("num_of_visits", 0)
+    # request.session["num_of_visits"] = num_visits + 1
+    context = {
+        # "num_of_books": num_of_books,
+        # "num_of_authors": num_of_authors,
+        # "num_of_literary_formats": num_of_literary_formats,
+        # "num_visits": num_visits + 1,
+    }
+    return render(request,"task_manager/index.html", context=context)
