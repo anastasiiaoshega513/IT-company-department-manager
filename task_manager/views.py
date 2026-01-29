4f65b76d8bbd65415bab7332c6262f5f936c99de
Steicy <anastasiaosega@gmail.com>
2026-01-29 21:41:56 +0200

    added task_list page

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,9 +1,13 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.views import generic
 
 from task_manager.models import Task, Worker
 
 
+@login_required
 def index(request: HttpRequest) -> HttpResponse:
     num_of_all_tasks = Task.objects.count()
     num_of_done_tasks = Task.objects.filter(is_completed=True).count()
@@ -16,3 +20,7 @@ def index(request: HttpRequest) -> HttpResponse:
         "num_of_workers": num_of_workers,
     }
     return render(request,"task_manager/index.html", context=context)
+
+
+class TaskListView(LoginRequiredMixin, generic.ListView):
+    model = Task
