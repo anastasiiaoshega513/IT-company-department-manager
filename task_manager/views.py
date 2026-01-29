d3267008f5f1fb4958f451c4bd35be7e79de2775
Steicy <anastasiaosega@gmail.com>
2026-01-29 20:51:57 +0200

    updated num_of_unfinished_tasks to num_of_active_tasks (sounds better)

--- task_manager/views.py
+++ task_manager/views.py
@@ -7,12 +7,12 @@ from task_manager.models import Task, Worker
 def index(request: HttpRequest) -> HttpResponse:
     num_of_all_tasks = Task.objects.count()
     num_of_done_tasks = Task.objects.filter(done=True).count()
-    num_of_unfinished_tasks = Task.objects.filter(done=False).count()
+    num_of_active_tasks = Task.objects.filter(done=False).count()
     num_of_workers = Worker.objects.count()
     context = {
         "num_of_all_tasks": num_of_all_tasks,
         "num_of_done_tasks": num_of_done_tasks,
-        "num_of_unfinished_tasks": num_of_unfinished_tasks,
+        "num_of_unfinished_tasks": num_of_active_tasks,
         "num_of_workers": num_of_workers,
     }
     return render(request,"task_manager/index.html", context=context)
