4f71502692472b60715c9eaec1d072c69c4fb116
Steicy <anastasiaosega@gmail.com>
2026-02-12 11:13:35 +0200

    change settings because of staticfiles problem (moved STATICFILES_DIRS)

--- IT_company_department_manager/settings/dev.py
+++ IT_company_department_manager/settings/dev.py
@@ -3,10 +3,6 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
-
 ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 # Database
